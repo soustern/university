9ba1060f78abb525a99695ea036c2b76d8ff77fa
unknown <rafaelantoniassivicechio@gmail.com>
2023-11-29 19:05:08 -0300

    add vercel change

--- PIM 4/app.py
+++ PIM 4/app.py
@@ -26,7 +26,7 @@ Session(app)
 db = SQL("sqlite:///finance.db")
 
 dic = {"rafael": {"nome": "Rafael Antoniassi Vicechio", "cpf": "51256824895", "cargo": "desenvolvedor", "salario": "10.000"}}
-
+dicname = {}
 
 @app.after_request
 def after_request(response):
@@ -81,35 +81,17 @@ def history():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+            for dic in dicname:
+                if ({"name": request.form.get("username"), "password": request.form.get("password")} == dicname[dic]):
+                    return redirect("/")
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?",
-                          request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -164,24 +146,8 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
-
-        # Ensure given username doesnt already exist at database
-        rows = db.execute("SELECT * FROM users")
-        for row in rows:
-            if username == row["username"]:
-                return apology("Username already exists", 400)
-
-        # Ensure given password and confirmation are equal
-        if confirmation != password:
-            return apology("Password and confirmation must be equal", 400)
-
-        # Define a hash value using the given password
-        hash = generate_password_hash(
-            password, method='pbkdf2:sha256', salt_length=8)
-
-        # Insert into database
-        db.execute(
-            "INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
+        
+        dicname[username] = {"name": username, "password": password}
 
         # Redirect user to login form
         return redirect("/")
